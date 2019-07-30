7ff1917d2237fd781b3c454c0b726483826d8511
Hong Min <hong@alina.ai>
2019-07-30 17:10:41 +0900

    Create HTML structure for ranking.jsx

--- src/pages/platform/ranking.jsx
+++ src/pages/platform/ranking.jsx
@@ -2,11 +2,41 @@
 import React from 'react';
 
 const Ranking = () => {
-    return (
-      <div className="container">
-        title
-      </div>
+  let rows_test = [
+    { rank: 1, title: 'Hong', score: 100 },
+    { rank: 2, title: 'Pikachu', score: 90 },
+  ];
+
+  const renderData = () => {
+    const { rank, title, score } = rows_test
+    return(
+      <tr key={rank}>
+        <td>{rank}</td>
+        <td>{title}</td>
+        <td>{score}</td>
+      </tr>
     )
-  }
+  } 
+  
+  return (
+    <div className="container">
+      <h1>RANK</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>name</th>
+            <th>score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>1</td>
+          <td>Hong</td>
+          <td>100</td>
+        </tbody>
+      </table>
+    </div>
+  )
+}
   
 export default Ranking;
