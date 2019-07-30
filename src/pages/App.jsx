4e547760382e494ffdfc6da048c3d871c376319e
Daewoong Ko <daewoong@alina.ai>
2019-07-30 14:11:21 +0900

    Add initial settings

---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+// Copyright (C) 2019 Alina Inc. All rights reserved.
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      안녕 알리나
+    </div>
+  );
+};
+
+export default App;
