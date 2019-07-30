d4e9fde981bd205075356d5d9dddf564d2923e46
Hong Min <hong@alina.ai>
2019-07-30 17:26:47 +0900

    Change file name to Ranking.jsx from ranking.jsx

--- src/pages/platform/ranking.jsx
+++ src/pages/platform/ranking.jsx
@@ -1,42 +1,7 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 import React from 'react';
 
-const Ranking = () => {
-  let rows_test = [
-    { rank: 1, title: 'Hong', score: 100 },
-    { rank: 2, title: 'Pikachu', score: 90 },
-  ];
+const Ranking = () => {}
 
-  const renderData = () => {
-    const { rank, title, score } = rows_test
-    return(
-      <tr key={rank}>
-        <td>{rank}</td>
-        <td>{title}</td>
-        <td>{score}</td>
-      </tr>
-    )
-  } 
-  
-  return (
-    <div className="container">
-      <h1>RANK</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>name</th>
-            <th>score</th>
-          </tr>
-        </thead>
-        <tbody>
-          <td>1</td>
-          <td>Hong</td>
-          <td>100</td>
-        </tbody>
-      </table>
-    </div>
-  )
-}
-  
-export default Ranking;
+    
+  export default Ranking;
