084d7a9d0643d27828240e2667160ec0274191b9
Daewoong Ko <daewoong@alina.ai>
2019-07-30 17:55:24 +0900

    Fix merge conflict

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,17 +4,17 @@ import { Button } from 'reactstrap';
 import mainPage from './../messages/mainPage';
 
 const MainPage = () => {
-    const onClickButton = () => {
-        console.log(mainPage.button);
-    };
-    return (
-        <div>
-            <h2>{mainPage.title}</h2>
-            <Button type="button" onClick={onClickButton}>
-                {mainPage.button}
-            </Button>
-        </div>
-    );
+  const onClickButton = () => {
+    console.log(mainPage.button);
+  };
+  return (
+    <div>
+      {mainPage.title}
+      <Button type="button" onClick={onClickButton}>
+        {mainPage.button}
+      </Button>
+    </div>
+  );
 };
 
 export default MainPage;
