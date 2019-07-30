cf3daff64610fb615358763d006feea902cc3ae6
Daewoong Ko <daewoong@alina.ai>
2019-07-30 18:40:13 +0900

    Add making a room function

--- src/pages/platform/Entry.jsx
+++ src/pages/platform/Entry.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Input, Row } from 'reactstrap';
 
 import entry from './../../messages/entry';
+import { FirebaseDatabaseMutation } from '@react-firebase/database';
 
-const Entry = () => {
-    const onClickButton = () => {
-        console.log(entry.button);
-    };
-    return (
-        <div className="containers">
-            <h2>{entry.title}</h2>
-            <Row>
-                <Input placeholder={entry.nickName} />
-            </Row>
-            <Row>
-                <Button type="button" onClick={onClickButton}>
-                    {entry.button}
-                </Button>
-            </Row>
-        </div>
-    );
+const Entry = ({ history, match }) => {
+  const inputRef = useRef(null);
+  return (
+    <div className="containers">
+      <h2>{entry.title}</h2>
+      <Row>
+        <Input innerRef={inputRef} placeholder={entry.nickName} />
+      </Row>
+      <Row>
+        <FirebaseDatabaseMutation path="/rooms/" type="push">
+          {({ runMutation }) => (
+            <Button type="button" onClick={async () => {
+              const res = await runMutation({ players: { nickname: inputRef.current.value } });
+              history.push(`waiting_room/${res.key}`);
+              }}>
+              {entry.button}
+            </Button>
+          )}
+        </FirebaseDatabaseMutation>
+      </Row>
+    </div>
+  );
 }
 
 export default Entry;
