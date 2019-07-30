fe5e9f42462961211297978381c2ac06c06f9c1b
Hong Min <hong@alina.ai>
2019-07-30 17:34:37 +0900

    [feat] Create dynamic table for ranking

--- src/pages/platform/Ranking.jsx
+++ src/pages/platform/Ranking.jsx
@@ -1,7 +1,44 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 import React from 'react';
 
-const Ranking = () => {}
+const Ranking = () => {
+    let rows_test = [
+    { rank: 1, title: 'Hong', score: 100 },
+    { rank: 2, title: 'Pikachu', score: 90 },
+  ];
+
+  const renderData = () => {
+    return rows_test.map((user, idx) => {
+      const { rank, title, score } = user
+      return(
+        <tr key={rank}>
+          <td>{rank}</td>
+          <td>{title}</td>
+          <td>{score}</td>
+        </tr>
+      )
+    })
+  }
+
+  return (
+    <div className="container">
+      <h1>RANK</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>name</th>
+            <th>score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderData()}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
     
-  export default Ranking;
+    
+export default Ranking;
