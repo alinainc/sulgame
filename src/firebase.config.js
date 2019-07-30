6590902f194803ef2b82791e9ad73f8c65c3abbd
Daewoong Ko <daewoong@alina.ai>
2019-07-30 17:25:25 +0900

    Add firebase config file

---
+++ src/firebase.config.js
@@ -0,0 +1,6 @@
+export default {
+  apiKey: "AIzaSyCW_cK5HZqsgv148ZVIBQnIsMYzxNJHYdw",
+  projectId: "alina-game",
+  databaseURL: "https://alina-game.firebaseio.com/",
+  authDomain: "alina-game.firebaseio.com",
+};
