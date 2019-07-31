7bf27730952673dc52746d48538b0ba333b64321
Daewoong Ko <daewoong@alina.ai>
2019-07-31 14:47:21 +0900

    Fix merge conflict

--- src/pages/platform/Entry.jsx
+++ src/pages/platform/Entry.jsx
@@ -5,11 +5,15 @@ import { Button, Input, Row } from 'reactstrap';
 
 import { FirebaseDatabaseMutation } from '@react-firebase/database';
 
-import shapes from '../../shapes';
 import entry from '../../messages/entry';
+import shapes from '../../shapes';
 
 const Entry = ({ history }) => {
   const inputRef = useRef(null);
+  const makeRoom = runMutation => async () => {
+    const res = await runMutation({ players: { nickname: inputRef.current.value } });
+    history.push(`waiting_room/${res.key}`);
+  };
   return (
     <div className="containers">
       <h2>{entry.title}</h2>
@@ -21,10 +25,7 @@ const Entry = ({ history }) => {
           {({ runMutation }) => (
             <Button
               type="button"
-              onClick={async () => {
-                const res = await runMutation({ players: { nickname: inputRef.current.value } });
-                history.push(`waiting_room/${res.key}`);
-              }}
+              onClick={makeRoom(runMutation)}
             >
               {entry.button}
             </Button>
