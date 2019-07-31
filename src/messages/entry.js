3f94e8c2809c1a25eea3a4f6379d05efccafa11c
Daewoong Ko <daewoong@alina.ai>
2019-07-31 20:27:17 +0900

    Fix messages in entry.js

--- src/messages/entry.js
+++ src/messages/entry.js
@@ -1,12 +1,10 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 
 export default {
-  entry: {
-    button: '입장하기',
-    nickName: '닉네임',
-    room: {
-      not: '방이 존재하지 않습니다',
-    },
-    title: '입장하기',
+  button: '입장하기',
+  nickName: '닉네임',
+  room: {
+    not: '방이 존재하지 않습니다',
   },
+  title: '입장하기',
 };
