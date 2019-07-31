d0b2eb13b28c15d870bad757d659c446d4ba20a4
Jusung Han <jusung@alina.ai>
2019-07-31 18:39:21 +0900

    fix game message files

--- src/messages/game1.js
+++ src/messages/game1.js
@@ -4,4 +4,4 @@ export default {
   title: "press",
   name: "눌러라",
   description: "제한 시간내에 버튼을 최대한 많이 누르세요",
-}
+};
--- src/messages/game2.js
+++ src/messages/game2.js
@@ -4,4 +4,4 @@ export default {
   title: "sequence",
   name: "맞춰라",
   description: "숫자를 최대한 빠르게 순서대로 맞추세요",
-}
+};
