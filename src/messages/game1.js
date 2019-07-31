d20618ef8d1c96f24d563acb84f573a821124bd2
Jusung Han <jusung@alina.ai>
2019-07-31 18:29:15 +0900

    add index and game messages

---
+++ src/messages/game1.js
@@ -0,0 +1,7 @@
+// Copyright (C) 2019 Alina Inc. All rights reserved.
+
+export default {
+  title: "press",
+  name: "눌러라",
+  description: "제한 시간내에 버튼을 최대한 많이 누르세요",
+}
---
+++ src/messages/index.js
@@ -0,0 +1,27 @@
+// Copyright (C) 2019 Alina Inc. All rights reserved.
+
+import entry from './entry';
+import game1 from './game1';
+import game2 from './game2';
+import mainPage from './mainPage';
+import ranking from './ranking';
+
+const games = {game1, game2};
+
+export {
+  entry,
+  game1,
+  game2,
+  games,
+  mainPage,
+  ranking,
+};
+
+export default {
+  entry,
+  game1,
+  game2,
+  games,
+  mainPage,
+  ranking,
+};
