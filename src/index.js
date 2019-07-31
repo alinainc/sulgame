e1669a3ed38994cf134b2ce0b917c0405b0a040b
Hong Min <hong@alina.ai>
2019-07-31 22:09:39 +0900

    [fix] Fix lint error

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './pages/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
