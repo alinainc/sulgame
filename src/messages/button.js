f477f06e27d957b8a3471c13a1609439f72b0206
Hong Min <hong@alina.ai>
2019-07-31 18:33:57 +0900

    [feat] Add button messages

---
+++ src/messages/button.js
@@ -0,0 +1,9 @@
+// Copyright (C) 2019 Alina Inc. All rights reserved.
+export default {
+  quit: '나가기',
+  retry: {
+    othergame: '게임고르기',
+    thisgame: '다시하기',
+  },
+  start: '시작!',
+};
--- src/messages/index.js
+++ src/messages/index.js
@@ -1,14 +1,16 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 
+import button from './button';
 import entry from './entry';
 import game1 from './game1';
 import game2 from './game2';
 import mainPage from './mainPage';
 import ranking from './ranking';
 
-const games = {game1, game2};
+const games = { game1, game2 };
 
 export {
+  button,
   entry,
   game1,
   game2,
