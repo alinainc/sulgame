1aca56a5b3635a829cdc50bd699ddf3d7d8c44a0
Hong Min <hong@alina.ai>
2019-07-31 21:01:58 +0900

    [feat] quit feature in ranking page
    go to mainpage

--- src/pages/platform/Ranking.jsx
+++ src/pages/platform/Ranking.jsx
@@ -4,8 +4,9 @@ import React from 'react';
 import { Button, Container, Table } from 'reactstrap';
 
 import { button, ranking } from '../../messages';
+import shapes from '../../shapes';
 
-const Ranking = () => {
+const Ranking = ({ history }) => {
   const rowsTest = [ // FIXME dummy data should be exchaged by real data
     { rank: 1, score: 100, title: 'Hong' },
     { rank: 2, score: 90, title: 'Pikachu' },
@@ -27,10 +28,12 @@ const Ranking = () => {
     })
   );
 
+  const goMain = () => history.push('/');
+
   return (
     <Container>
       <h1>{ranking.title}</h1>
-      <Button>{button.quit}</Button>
+      <Button onClick={goMain}>{button.quit}</Button>
       <Table hover>
         <thead>
           <tr>
@@ -47,6 +50,10 @@ const Ranking = () => {
       <Button>{button.retry.thisgame}</Button>
     </Container>
   );
+};
+
+Ranking.propTypes = {
+  history: shapes.history.isRequired,
 };
 
 export default Ranking;
