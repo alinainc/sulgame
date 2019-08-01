588e2bb45f58172590eaf2de6ab6bd87f569a7c7
Jusung Han <jusung@alina.ai>
2019-08-01 17:56:02 +0900

    Add rank logic for game 2

--- src/pages/components/RankLogic.jsx
+++ src/pages/components/RankLogic.jsx
@@ -5,6 +5,9 @@ const RankLogic = (items) => {
     case 1:
       items.sort((a, b) => (parseFloat(b.gameData) - parseFloat(a.gameData)));
       break;
+    case 2:
+      items.sort((a, b) => (parseFloat(a.gameData) - parseFloat(b.gameData)));
+      break;
     default:
       items.sort((a, b) => (parseFloat(b.gameData) - parseFloat(a.gameData)));
   }
