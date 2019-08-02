c09f46ba97d987e6fc9351505fa355de1ecb8f60
Hong Min <hong@alina.ai>
2019-08-02 18:59:07 +0900

    [refactor] Change selector name
    this is for main page not for entry page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -15,7 +15,7 @@ const MainPage = ({ history }) => {
       <Container>
         <h1 className="center-horizental">{mainPage.title}</h1>
       </Container>
-      <Button type="button" onClick={onClickButton} className="bottom-right btn circle entry">
+      <Button type="button" onClick={onClickButton} className="bottom-right btn circle main">
         {mainPage.button}
       </Button>
     </Container>
