177862528f9c6ab16658d977d1faed7ffc04e98b
Daewoong Ko <daewoong@alina.ai>
2019-08-05 13:45:13 +0900

    Add rated color

--- src/pages/components/Rating.jsx
+++ src/pages/components/Rating.jsx
@@ -5,17 +5,22 @@ import Ratings from 'react-ratings-declarative';
 
 const Rating = () => {
   const [state, setState] = useState(1);
+  const options = {
+    widgetDimension: '30px',
+    widgetHoverColor: '#f3d076',
+    widgetRatedColor: '#f3d076',
+  };
   return (
     <Ratings
       rating={state.rating}
       widgetRatedColors="blue"
       changeRating={e => setState({ rating: e })}
     >
-      <Ratings.Widget widgetDimension="30px" widgetHoverColor="#f3d076" />
-      <Ratings.Widget widgetDimension="30px" widgetHoverColor="#f3d076" />
-      <Ratings.Widget widgetDimension="30px" widgetHoverColor="#f3d076" />
-      <Ratings.Widget widgetDimension="30px" widgetHoverColor="#f3d076" />
-      <Ratings.Widget widgetDimension="30px" widgetHoverColor="#f3d076" />
+      <Ratings.Widget {...options} />
+      <Ratings.Widget {...options} />
+      <Ratings.Widget {...options} />
+      <Ratings.Widget {...options} />
+      <Ratings.Widget {...options} />
     </Ratings>
   );
 };
