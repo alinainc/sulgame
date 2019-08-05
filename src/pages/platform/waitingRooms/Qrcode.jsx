622a55c72dc1a93498a358ac5a99c7fd59dcabb3
Hong Min <hong@alina.ai>
2019-08-05 17:06:28 +0900

    [feat] Create Qrcode component

---
+++ src/pages/platform/waitingRooms/Qrcode.jsx
@@ -0,0 +1,24 @@
+// Copyright (C) 2019 Alina Inc. All rights reserved.
+
+import PropTypes from 'prop-types';
+import QRCode from 'qrcode.react';
+import React from 'react';
+import { Container, Row } from 'reactstrap';
+
+const Qrcode = ({ roomId }) => (
+  <Container>
+    <Row className="divider">QR code</Row>
+    <Row>
+      <QRCode value={`localhost:3000/platform/entry/${roomId}`} />
+    </Row>
+  </Container>
+);
+
+Qrcode.propTypes = {
+  roomId: PropTypes.string,
+};
+Qrcode.defaultProps = {
+  roomId: 'host',
+};
+
+export default Qrcode;
