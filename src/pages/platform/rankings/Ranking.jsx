0ac62292895c0d1eb879ff45804fbe794922b2d0
Daewoong Ko <daewoong@alina.ai>
2019-08-05 11:39:16 +0900

    Fix bug

--- src/pages/platform/rankings/Ranking.jsx
+++ src/pages/platform/rankings/Ranking.jsx
@@ -23,9 +23,9 @@ const Ranking = ({ history, isHost, match: { params: { roomId, userId } } }) => 
         }
         if (value.players.host.replay === 1) {
           if (!userId) {
-            return <Redirect to={`/clickgame/play/${roomId}/user/host`} />;
+            return <Redirect to={`/games/click/play/${roomId}/user/host`} />;
           }
-          return <Redirect to={`/clickgame/play/${roomId}/user/${userId}`} />;
+          return <Redirect to={`/games/click/play/${roomId}/user/${userId}`} />;
         }
         if (!value.players.host.start) {
           return <Redirect to={`/platform/waiting_room/${roomId}/user/${userId}`} />;
