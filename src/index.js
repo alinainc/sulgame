a3f5ef6026ecd1dffc0c1581aa188fa0ef4b33ee
Daewoong Ko <daewoong@alina.ai>
2019-08-06 18:00:43 +0900

    Add function to prevent zoom when button being fast tapped

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import initReactFastclick from 'react-fastclick';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+initReactFastclick();
