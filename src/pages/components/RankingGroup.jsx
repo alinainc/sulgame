650ed6da128c23efa01c751c693324ad725bc145
Hong Min <hong@alina.ai>
2019-08-06 21:03:54 +0900

    [feat] Create RankingGroup for golala

---
+++ src/pages/components/RankingGroup.jsx
@@ -0,0 +1,20 @@
+// Copyright (C) 2019 Alina Inc. All rights reserved.
+
+import React from 'react';
+
+const RankingGroup = () => (
+  <table>
+    <thead>
+      <th>A</th>
+      <th>B</th>
+    </thead>
+    <tbody>
+      {/* FIXME 이 아래에 반복문작성 */}
+      <tr>
+        <td>person name here</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+export default RankingGroup;
