3568605c96b58bb640aa57b71e41f8acdba5c7e6
Hong Min <hong@alina.ai>
2019-08-06 11:15:14 +0900

    [feat] Player component for waiting page

--- src/pages/components/PlayerList.jsx
+++ src/pages/components/PlayerList.jsx
@@ -1,68 +1,45 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 
-import { get, remove } from 'lodash';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Col, Container, Row, Spinner } from 'reactstrap';
+import { Spinner } from 'reactstrap';
 
-import { ranking } from '../../messages';
-import RankLogic from './RankLogic';
-
-const PlayerList = ({ cols, isRank, value }) => {
+const PlayerList = ({ value }) => {
   let items;
 
   if (value) {
     if (value.players) {
       items = Object.values(value.players);
-      remove(items, ({ connect }) => (connect === 0));
-      if (isRank) {
-        RankLogic(items);
-      }
     }
   }
 
   return (
-    <Container className="section">
-      <Row className="bar">
-        {cols.map(col => <Col key={col.name} xs={col.xsHead}>{col.name}</Col>)}
-      </Row>
-      {!value
-        ? <Spinner color="primary" />
-        : items.map((item, i) => (
-          <Row key={item.name || item.nickname} className="players">
-            {cols.map(col => (
-              <Col key={col.name} xs={col.xsChild}>
-                <div>
-                  <span role="img" aria-label="face" className="face"> 🧞‍</span>
-                </div>
-                <div>
-                  {col.key === 'rank' ? `${(i + 1)} ${ranking.rank.postfix}` : get(item, col.key, ' ')}
-                </div>
-              </Col>
-            ))}
-          </Row>
+    <div className="section">
+      <div className="bar">
+        <span>플레이어</span>
+      </div>
+      {!items
+        ? <Spinner color="primary" /> : items.map(item => (
+          <div key={item.name} className="players">
+            <div>
+              <span role="img" aria-label="face" className="face"> 🧞‍</span>
+            </div>
+            <div>
+              {item.name}
+            </div>
+          </div>
         ))}
-    </Container>
+    </div>
   );
 };
 
 PlayerList.propTypes = {
-  cols: PropTypes.arrayOf(PropTypes.shape({
-    className: PropTypes.string,
-    key: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    xsChild: PropTypes.number,
-    xsHead: PropTypes.number,
-  })),
-  isRank: PropTypes.bool,
   value: PropTypes.shape({
     players: PropTypes.shape({}),
   }),
 };
 
 PlayerList.defaultProps = {
-  cols: [],
-  isRank: undefined,
   value: undefined,
 };
 
