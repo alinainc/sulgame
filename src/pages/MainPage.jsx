3132919a6f4004988e9623e75730b3a9787570f0
Hong Min <hong@alina.ai>
2019-08-06 15:57:14 +0900

    [refactor] Set mainpage style at mainpage style sheet

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,6 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 
 import React from 'react';
-import { Button, Container } from 'reactstrap';
 
 import mainPage from '../messages/mainPage';
 import shapes from '../shapes';
@@ -11,20 +10,20 @@ const MainPage = ({ history }) => {
     history.push('/platform/entry');
   };
   return (
-    <Container className="mainpage">
-      <Container>
-        <div className="block center y moon" />
-        <h1 className="center y inline">{mainPage.title}</h1>
-      </Container>
-      <Container className="center bottom y">
-        <div className="block center y">
-          <div className="center y inline">{mainPage.button}</div>
+    <div className="mainpage">
+      <div>
+        <div className="moon" />
+        <h1>{mainPage.title}</h1>
+      </div>
+      <div className="bottom center">
+        <div>
+          <div>{mainPage.button}</div>
         </div>
-        <Button type="button" onClick={onClickButton} className="block center y">
+        <button type="button" onClick={onClickButton}>
           <span>+</span>
-        </Button>
-      </Container>
-    </Container>
+        </button>
+      </div>
+    </div>
   );
 };
 
