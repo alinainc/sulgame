ae552259d7665d46d1204183644113e499f22049
Hong Min <hong@alina.ai>
2019-08-07 14:51:53 +0900

    [feat] Style for invite components

--- src/pages/platform/waitingRooms/UrlCopy.jsx
+++ src/pages/platform/waitingRooms/UrlCopy.jsx
@@ -8,19 +8,6 @@ import domain from '../../../domain.config';
 import { button, waitingRoom } from '../../../messages';
 
 const UrlCopy = ({ roomId }) => (
-<<<<<<< Updated upstream
-  <div className="section urlcopy">
-    <div className="bar">{`${waitingRoom.url}`}</div>
-    <div className="warper">
-      <textarea defaultValue={`${domain.default}/platform/entry/${roomId}`} disabled />
-      <CopyToClipboard text={`http://${domain.default}/platform/entry/${roomId}`}>
-        <button type="button" className="contents">
-          <span>{button.copy}</span>
-        </button>
-      </CopyToClipboard>
-    </div>
-  </div>
-=======
   <table>
     <thead>
       <tr>
@@ -40,10 +27,8 @@ const UrlCopy = ({ roomId }) => (
           </CopyToClipboard>
         </td>
       </tr>
-      
     </tbody>
   </table>
->>>>>>> Stashed changes
 );
 
 UrlCopy.propTypes = {
