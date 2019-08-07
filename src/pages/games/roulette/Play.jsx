6c14c67ac29452a5409c34cd3f668b0b08428f06
Jusung Han <jusung@alina.ai>
2019-08-07 16:37:35 +0900

    Add loading page to roulette game

--- src/pages/games/roulette/Play.jsx
+++ src/pages/games/roulette/Play.jsx
@@ -1,22 +1,27 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FirebaseDatabaseMutation, FirebaseDatabaseNode } from '@react-firebase/database';
 
-import { button } from '../../../messages';
+import { button, rouletteGame } from '../../../messages';
 import shapes from '../../../shapes';
+import Ready from '../../components/Ready';
 import Roulette from './Roulette';
 
-const Play = ({ history, match: { params: { roomId } } }) => {
+const Play = ({ history, match: { params: { roomId, userId } } }) => {
+  const [gameStart, setGameStart] = useState(false);
+
   const toWaiting = () => (
     <FirebaseDatabaseMutation path={`/rooms/${roomId}/players/host`} type="update">
       {({ runMutation }) => (
         <button
           type="button"
           onClick={() => {
-            history.push(`/platform/waiting_room/${roomId}/host`);
-            runMutation({ start: 0 });
+            if (userId === 'host') {
+              history.push(`/platform/waiting_room/${roomId}/host`);
+              runMutation({ start: 0 });
+            }
           }}
         >
           {button.retry.othergame}
@@ -27,20 +32,30 @@ const Play = ({ history, match: { params: { roomId } } }) => {
   const handleOnComplete = value => console.log(value);
   const getName = obj => Object.values(obj).map(e => e.name);
 
+
   return (
-    <FirebaseDatabaseNode path={`/rooms/${roomId}/players`}>
-      {({ value }) => {
-        if (!value) {
-          return null;
-        }
-        return (
-          <div className="roulette-container">
+    <div className="roulette-container">
+      <FirebaseDatabaseNode path={`/rooms/${roomId}/players`}>
+        {({ value }) => {
+          if (!value) {
+            return null;
+          }
+          setGameStart(true);
+          return (
             <Roulette options={getName(value)} baseSize={150} onComplete={handleOnComplete} />
-            {toWaiting()}
-          </div>
-        );
-      }}
-    </FirebaseDatabaseNode>
+          );
+        }}
+      </FirebaseDatabaseNode>
+      {!gameStart
+        ? (
+          <Ready
+            description={rouletteGame.description}
+            title={rouletteGame.title}
+          />
+        )
+        : toWaiting()
+      }
+    </div>
   );
 };
 
