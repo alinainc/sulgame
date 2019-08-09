679f1c417f63184e3bb8e8553e562681b3b21ef5
Daewoong Ko <daewoong@alina.ai>
2019-08-09 23:13:40 +0900

    Add messages in feedback.js

--- src/messages/feedback.js
+++ src/messages/feedback.js
@@ -4,7 +4,10 @@ export default {
   contents1: '아니면 부족한 부분이 많았나요?',
   contents2: '여러분들의 피드백은 언제나 소중합니다.',
   contents3: '솔직한 의견을 별점과 함께 보내주세요!',
-  email: 'email을 입력해 주세요',
+  email: 'email을 입력해 주세요 (선택)',
   end: '더 나은 술 게임이 되도록 만들겠습니다!',
+  nocontents: '내용을 입력해주세요',
+  norating: '별점을 선택해주세요',
   title: '술 게임 재미있게 즐기셨나요?',
+  type: '내용을 입력해주세요',
 };
