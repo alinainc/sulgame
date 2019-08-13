570febdfe9ad93165d24b4d90288174e52e9246c
Daewoong Ko <daewoong@alina.ai>
2019-08-13 17:42:08 +0900

    Add messages

--- src/i18n/messages/waitingRoom.js
+++ src/i18n/messages/waitingRoom.js
@@ -1,9 +1,17 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 
 export default {
+  description: {
+    one: 'QR Code를 공유해서 다른 플레이어를 초대하세요',
+    two: '복사한 URL을 다른 플레이어에게 보내서 초대하세요',
+  },
   games: '게임 목록',
   invite: '초대하기',
   nouser: '해당 유저가 존재하지 않습니다.',
+  option: {
+    one: '방법 1',
+    two: '방법 2',
+  },
   players: {
     count: '명',
     title: '플레이어',
--- src/pages/platform/waitingRooms/UrlCopy.jsx
+++ src/pages/platform/waitingRooms/UrlCopy.jsx
@@ -1,15 +1,16 @@
 // Copyright (C) 2019 Alina Inc. All rights reserved.
 
 import PropTypes from 'prop-types';
-import React from 'react';
-import { useIntl } from 'react-intl';
+import React, { useRef } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useIntl } from 'react-intl';
 
 import domain from '../../../domain.config';
 import { messages, t } from '../../../i18n';
 
 const UrlCopy = ({ roomId }) => {
   const intl = useIntl();
+
   return (
     <div>
       <h3>
