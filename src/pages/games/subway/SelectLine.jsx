6c95cc5ab0851fb73259170c14fb2198fb9a8228
Daewoong Ko <daewoong@alina.ai>
2019-08-20 11:44:18 +0900

    Add gyeongui line

--- src/pages/games/subway/SelectLine.jsx
+++ src/pages/games/subway/SelectLine.jsx
@@ -15,7 +15,7 @@ const SelectLine = ({ history, match: { params: { roomId, userId } } }) => {
   const intl = useIntl();
   const lines = [
     'line1', 'line2', 'line3', 'line4', 'line5',
-    'line6', 'line7', 'line8', 'line9',
+    'line6', 'line7', 'line8', 'line9', 'gyeongui',
   ];
   useEffect(() => {
     if (userId === 'host') {
