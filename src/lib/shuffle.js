19b7308e380a6631bc7e96e268cf13300e49068e
Daewoong Ko <daewoong@alina.ai>
2019-08-22 18:29:53 +0900

    Add the function that shuffle elements of an array

---
+++ src/lib/shuffle.js
@@ -0,0 +1,11 @@
+// Copyright (C) 2019 Alina Inc. All rights reserved.
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
+
+export default (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
